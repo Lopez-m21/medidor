c88964420e2ecba80cbd3849b297981cd02a5bf4
Lopez-m21 <divadboyy@gmail.com>
2022-05-24 11:33:27 -0500

    Cambio de API

--- fire.js
+++ fire.js
@@ -2,14 +2,14 @@ var firebase = require('firebase')
 // change lines below with your own Firebase snippets!
 var config = {
 
-  apiKey: "AIzaSyBeZZte8NoLdzj5eYd26QnICidQoP85B8c",
-  authDomain: "region676-e21b8.firebaseapp.com",
-  databaseURL: "https://region676-e21b8.firebaseio.com",
-  projectId: "region676-e21b8",
-  storageBucket: "region676-e21b8.appspot.com",
-  messagingSenderId: "764985918459",
-  appId: "1:764985918459:web:4c38f754eda41b3476c15e",
-  measurementId: "G-W1QHXKKH5R"
+  apiKey: "AIzaSyBuOE4Z7QFu5GN_hYPHSosG_V8gDoPXwoI",
+  authDomain: "medidordeagua-f00de.firebaseapp.com",
+  databaseURL: "https://medidordeagua-f00de-default-rtdb.firebaseio.com",
+  projectId: "medidordeagua-f00de",
+  storageBucket: "medidordeagua-f00de.appspot.com",
+  messagingSenderId: "693057292616",
+  appId: "1:693057292616:web:4d981c2a700edeb1474b7d",
+  measurementId: "G-QBKFMKPY1L"
 };
 const fire = firebase.initializeApp(config);
 module.exports = fire;
