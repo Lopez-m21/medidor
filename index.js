f456328f0b24bb5adc73d64478276263c1a44929
Lopez-m21 <divadboyy@gmail.com>
2022-05-24 11:48:34 -0500

    Cambio de variables 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/ver', (req, res) => {
       timestampsInSnapshots: true
     });
     var wholeData = []
-	db.collection('Lecturas').orderBy('fecha', 'asc').get()
+	db.collection('Lecturas').orderBy('Fecha', 'asc').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
       
